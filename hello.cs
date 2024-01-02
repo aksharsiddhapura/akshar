e46d166d4323f204fcac64e3b858ea9ba5c0cb60
aksharsiddhapura <155410895+aksharsiddhapura@users.noreply.github.com>
2024-01-02 09:21:55 +0530

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.Write("Hello Word");
+            Console.Read();
+        }
+    }
+}
